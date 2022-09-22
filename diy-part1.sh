15dfa0515875ead41a9c4d5ee2b981db44965ce3
leshanydy2022 <98255172+leshanydy2022@users.noreply.github.com>
2022-09-22 13:11:26 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,3 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-git clone -b luci --single-branch https://github.com/xiaorouji/openwrt-passwall.git feeds/luci/applications/luci-app-passwall
