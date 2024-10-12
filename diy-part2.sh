930d84dee28eef23a6dd346f5f110eb7200b65b9
leshanydy2022 <98255172+leshanydy2022@users.noreply.github.com>
2024-10-12 20:14:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -22,8 +22,8 @@ sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_gener
 #git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns
 rm -rf feeds/luci/applications/luci-app-smartdns
 git clone https://github.com/leshanydy2022/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
-rm -rf feeds/other/luci-app-adguardhome
-git clone https://github.com/leshanydy2022/luci-app-adguardhome.git feeds/other/luci-app-adguardhome
+#rm -rf feeds/other/luci-app-adguardhome
+#git clone https://github.com/leshanydy2022/luci-app-adguardhome.git feeds/other/luci-app-adguardhome
 # 替换默认主题为Modify default theme（FROM luci-theme-bootstrap CHANGE TO luci-theme-bootstrap-mod）
 sed -i 's/luci-theme-bootstrap/luci-theme-bootstrap-mod/g' ./feeds/luci/collections/luci/Makefile
 #修改活动连接数
@@ -31,8 +31,8 @@ sed -i 's/luci-theme-bootstrap/luci-theme-bootstrap-mod/g' ./feeds/luci/collecti
 #修改 Linux 内核版本-注意 Makefile 文件的路径
 #sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=5.19/g' ./target/linux/x86/Makefile
 # 为adguardhome插件更换最新的版本
-rm -rf feeds/packages/net/adguardhome
-git clone https://github.com/leshanydy2022/adguardhome.git feeds/packages/net/adguardhome
+#rm -rf feeds/packages/net/adguardhome
+#git clone https://github.com/leshanydy2022/adguardhome.git feeds/packages/net/adguardhome
 # 为smartDNS插件更换最新的版本
 rm -rf feeds/packages/net/smartdns
 #git clone https://github.com/pymumu/openwrt-smartdns.git feeds/packages/net/smartdns
